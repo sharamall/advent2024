49aad3fac8afc6834290680cead5ba163dbd02c1
sharamall <sharamall@hotmail.com>
2024-12-01 17:19:08 -0500

    day1part2

---
+++ day1part2.rb
@@ -0,0 +1,16 @@
+require 'byebug'
+
+file = File.open "day1in.txt"
+
+a = []
+b = []
+file.each_line do |l|
+  erm1, erm2 = l.split(" ").filter { |it| it != " " }
+  a << erm1.to_i
+  b << erm2.to_i
+end
+sim = 0
+a.each do |arg|
+  sim += arg * b.count { |it| it == arg}
+end
+puts sim
